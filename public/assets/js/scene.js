aca629a642deff008b7d3aa037ff9a2d72806d3d
Loïc Horellou <hello@loichorellou.net>
2018-05-14 21:48:41 +0200

    update scene JS + CSS

--- public/assets/js/scene.js
+++ public/assets/js/scene.js
@@ -95,6 +95,31 @@ function loadPrototypo(email,password, fontName, fontVariant, sock){
 }
 
 
+
+window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+                              window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+
+var start = null;
+
+var d = document.getElementById("scene");
+
+function step(timestamp) {
+	var progress;
+	if (start === null) start = timestamp;
+	progress = timestamp - start;
+	d.style.left = Math.min(progress/10, 200) + "px";
+	
+	if (progress < 2000) {
+		requestAnimationFrame(step);
+	}
+}
+
+
+
+requestAnimationFrame(step);
+
+
 $(function () {
 	var socket = io();
 
